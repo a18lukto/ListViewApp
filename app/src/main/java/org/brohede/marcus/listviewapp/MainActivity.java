2ec51c24a2020222b6d73f383671999cf90e314c
a18lukto <a18lukto@student.his.se>
2019-04-09 16:35:50 +0200

    Array till listview

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,8 +2,11 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String [] rawData = {"leif", "ulla", "kjell"};
+
+        List<String> listData = new ArrayList<String>(Arrays.asList(rawData));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview,R.id.my_item_textview, listData);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        adapter.add("Lukas");
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
         // will hold/show your data created here. You need three create things:
