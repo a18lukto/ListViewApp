12a1a23ef090162266b7b81fe98671ffe58c6865
a18lukto <a18lukto@student.his.se>
2019-04-16 14:27:23 +0200

    Fungerande Toast när man trycker på något i listan

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -1,9 +1,13 @@
 package org.brohede.marcus.listviewapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    private ArrayList<String> listData;
 
 
     @Override
@@ -21,15 +26,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listData = new ArrayList<>(Arrays.asList(mountainNames));
 
-        List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
+        //List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
 
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview,R.id.my_item_textview, listData);
+        //ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview,R.id.my_item_textview, listData);
 
-        ListView myListView = (ListView)findViewById(R.id.my_listview);
-        myListView.setAdapter(adapter);
+        //ListView myListView = (ListView)findViewById(R.id.my_listview);
+        //myListView.setAdapter(adapter);
 
-        adapter.add("Lukas");
+        //adapter.add("Lukas");
+
+        String[] gustav = new String[]{"Hej","På", "Dig!"};
+        Toast.makeText(getApplicationContext(),Arrays.toString(gustav),Toast.LENGTH_SHORT).show();
+
+      //  myListView.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+         //   public void onClick(View view) {
+           //     Toast.makeText(getApplicationContext(),"name:"+mountainNames[i],Toast.LENGTH_SHORT).show();
+         //   }
+       // });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item_textview,listData);
+
+        ListView my_listView=(ListView) findViewById(R.id.my_listview);
+
+        my_listView.setAdapter(adapter);
+        my_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"Name:  "+mountainNames[i]+"\n"+"Heigth:  "+mountainHeights[i]+"\n"+"Location:  "+mountainLocations[i],Toast.LENGTH_SHORT).show();
+            }
+        });
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
         // will hold/show your data created here. You need three create things:
